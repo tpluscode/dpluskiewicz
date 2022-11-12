e60e3cd3efc5609307214e2b496afa7ec8609efc
tpluscode <tpluscode@users.noreply.github.com>
2022-11-12 16:38:24 +0100

    use modern components

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 gem 'github-pages', group: :jekyll_plugins
 gem 'jekyll-sitemap', group: :jekyll_plugins
-gem 'jekyll-copyr', group: :jekyll_plugins
---
+++ assets/components.js
@@ -0,0 +1,10 @@
+import '@material/mwc-fab/mwc-fab.js'
+import '@openlayers-elements/core/ol-map.js'
+import '@openlayers-elements/maps/ol-overlay.js'
+import '@openlayers-elements/maps/ol-layer-openstreetmap.js'
+import '@openlayers-elements/core/ol-layer-vector.js'
+import '@openlayers-elements/maps/ol-marker-icon.js'
+
+addEventListener('load', function() {
+    document.body.classList.remove('unresolved');
+})
