56e744e9f4b0bfe559da59b0a70a73a2a4a53cf9
tpluscode <tpluscode@users.noreply.github.com>
2022-11-13 19:13:51 +0100

    defer components

--- assets/components.js
+++ assets/components.js
@@ -1,10 +1,22 @@
-import '@material/mwc-fab/mwc-fab.js'
-import '@openlayers-elements/core/ol-map.js'
-import '@openlayers-elements/maps/ol-overlay.js'
-import '@openlayers-elements/maps/ol-layer-openstreetmap.js'
-import '@openlayers-elements/core/ol-layer-vector.js'
-import '@openlayers-elements/maps/ol-marker-icon.js'
+import onetime from 'onetime'
 
-addEventListener('load', function() {
+import('@material/mwc-fab/mwc-fab.js').then(() => {
     document.body.classList.remove('unresolved');
 })
+
+const loadMap = onetime(async () => {
+    await import('@openlayers-elements/core/ol-map.js')
+    // import('@openlayers-elements/maps/ol-overlay.js')
+    import('@openlayers-elements/maps/ol-layer-openstreetmap.js')
+    import('@openlayers-elements/core/ol-layer-vector.js')
+    import('@openlayers-elements/maps/ol-marker-icon.js')
+})
+
+const mapObserver = new IntersectionObserver((changes) => {
+    if (changes.some(change => change.intersectionRatio > 0)) {
+        loadMap()
+    }
+})
+
+document.querySelectorAll('ol-map')
+    .forEach(el => mapObserver.observe(el))
